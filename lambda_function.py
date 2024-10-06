12d83262351e468e50e9fd2a313e6f9978ecbaa5
wyltele <57303708+wyltele@users.noreply.github.com>
2024-10-06 08:01:22 -0400

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -6,13 +6,4 @@ from Books2 import Renew
 
 
 def lambda_handler(event, context):
-    # TODO implement	
-
 	Renew().run()
-    # page_data = ""
-    # if 'url' in event.keys():
-        # driver.get(event['url'])
-        # page_data = driver.page_source
-        # print(page_data)
-    # driver.close()
-    # return page_data
