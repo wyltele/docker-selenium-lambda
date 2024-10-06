1afd34e9d4de5495fa1270dd6313097a0fe6be68
wyltele <57303708+wyltele@users.noreply.github.com>
2024-10-06 10:04:55 -0400

    Update renew_book.py

--- renew_book.py
+++ renew_book.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from datetime import datetime,timedelta
+from tempfile import mkdtemp
 import requests
 import time
 import os
